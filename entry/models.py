73d4ab474659a4eb20f706e7402dcf7029856332
sooraj1719 <surajchafale@gmail.com>
2014-12-13 11:29:51 +0530

    constraints are added for entry models. e.g. Berths should always be L/M/U/SL/SU etc

--- entry/models.py
+++ entry/models.py
@@ -3,12 +3,14 @@ import datetime
 # Create your models here.
 
 class Entries(models.Model):
+	berth_choices =(('L','Lower'),('M','Middle'),('U','Upper'),('SL','SideLower'),('SM','SideMiddle'),('SU','SideUpper'),('Others','Others'))
+	class_choices =(('1-AC','1-AC'),('2-AC','2-AC'),('3-AC','3-AC'),('SL','Sleeper'),('CC','CarChair'),('Others','Others'))
 	train_name = models.CharField(max_length=200)
 	date_of_journey= models.DateField()
 	from_station = models.CharField(max_length=4)
 	to_station = models.CharField(max_length=4)
-	class_selection = models.CharField(max_length=128)
-	berth_selection = models.CharField(max_length=128)
+	class_selection = models.CharField(max_length=6, choices=class_choices)
+	berth_selection = models.CharField(max_length=6, choices=berth_choices)
 	comments = models.CharField(max_length=2000)
 	
 	def __unicode__(self):
