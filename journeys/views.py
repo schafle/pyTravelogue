bf4ba61bcabd853cc639881f643cf3608f55660a
sooraj1719 <surajchafale@gmail.com>
2014-12-28 23:09:35 +0530

    Number of places visited and Number of journeys is calculated and shown.

--- journeys/views.py
+++ journeys/views.py
@@ -7,21 +7,23 @@ from entry.models import Entries
 from django.db.models import Count
 	
 def index(request):
-    # Request the context of the request.
-    # The context contains information such as the client's machine details, for example.
-    context = RequestContext(request)
+	# Request the context of the request.
+	# The context contains information such as the client's machine details, for example.
+	context = RequestContext(request)
 
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by no. likes in descending order.
-    # Retrieve the top 5 only - or all if less than 5.
-    # Place the list in our context_dict dictionary which will be passed to the template engine.
-    stations_list=Station.objects.all()
-    #journey_list=Entries.objects.annotate(num_trains=Count('train_name')).order_by('-num_trains')[:]
-    journey_list=Entries.objects.values('train_name').annotate(dcount=Count('train_name'))
-    berth_list=Entries.objects.values('berth_selection').annotate(count_berth=Count('berth_selection'))
-    context_dict = {'stations': stations_list, 'entries':journey_list, 'berths':berth_list}
-	
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    return render_to_response('journeys/index.html', context_dict, context)
+	# Query the database for a list of ALL categories currently stored.
+	# Order the categories by no. likes in descending order.
+	# Retrieve the top 5 only - or all if less than 5.
+	# Place the list in our context_dict dictionary which will be passed to the template engine.
+	stations_list=Station.objects.all()
+	#journey_list=Entries.objects.annotate(num_trains=Count('train_name')).order_by('-num_trains')[:]
+	journey_list=Entries.objects.values('train_name').annotate(dcount=Count('train_name'))
+	berth_list=Entries.objects.values('berth_selection').annotate(count_berth=Count('berth_selection'))
+	number_of_places=Entries.objects.values('to_station').distinct().count()
+	number_of_trains=Entries.objects.count()
+	context_dict = {'stations': stations_list, 'entries':journey_list, 'berths':berth_list, 'number_of_places':number_of_places, 'number_of_trains':number_of_trains}
+
+	# Return a rendered response to send to the client.
+	# We make use of the shortcut function to make our lives easier.
+	# Note that the first parameter is the template we wish to use.
+	return render_to_response('journeys/index.html', context_dict, context)
